383a9c71d6b811febcf009cdced533a63600facc
Pavel-Pro <proskpavel@gmail.com>
2018-10-23 18:35:42 +0300

    4

--- js/custom.js
+++ js/custom.js
@@ -13,6 +13,29 @@ $(function(){
 
 });
 
+
+$(function() {
+	$(".tabs a").click(function () {
+		$(this).parents(".tab-wrap").find(".tab-cont").addClass("hide");
+		$(this).parent().siblings().removeClass("active");
+		var id = $(this).attr("href");
+		$(id).removeClass("hide");
+		$(this).perent().addClass("active");
+		return false;
+	}); 
+
+	$(".banner-slider, .testimonial-slider").slick({
+		arrows: false,
+		dots: true,
+	})
+	$(".portfolio-slider").slick({
+		dots: true,
+		appendArrows: '.portfolio-slider_buttons',
+		prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+	})
+});
+
    
 
 
